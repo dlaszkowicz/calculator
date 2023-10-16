571a475358fbeb4f0021a3f399816e24bff832d0
dlaszkowicz <d.laszkowicz@gmail.com>
2023-10-16 14:03:23 +0200

    Design of the site Done

---
+++ script.js
@@ -0,0 +1,10 @@
+let first_num = 0;
+let second_num = 0;
+let operator;
+function operate(x, y, op) {
+  op(x, y);
+}
+function add(x, y) {}
+function substract(x, y) {}
+function multiply(x, y) {}
+function divide(x, y) {}
